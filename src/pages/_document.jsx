12f52c355a5567e8c0b595b326ad8ee18e355190
Norld <grymore@gmail.com>
2023-02-11 21:32:22 -0800

    g-analytics

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,9 +1,26 @@
 import { Head, Html, Main, NextScript } from 'next/document'
-
+import { Script } from 'next/script'
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
       <Head />
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-F9MX23Z3MM"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-F9MX23Z3MM', {
+            page_path: window.location.pathname,
+          });
+        `,
+        }}
+      />
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
