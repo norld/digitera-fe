c33039ed7c33cfd8ce703b055f10458371ea4a3c
Norld <grymore@gmail.com>
2023-02-11 21:37:59 -0800

    env

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,24 +3,25 @@ import { Script } from 'next/script'
 export default function Document() {
   return (
     <Html className="h-full bg-gray-50 antialiased" lang="en">
-      <Head />
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-F9MX23Z3MM"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-F9MX23Z3MM', {
-            page_path: window.location.pathname,
-          });
-        `,
-        }}
-      />
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
       <body className="flex h-full flex-col">
         <Main />
         <NextScript />
