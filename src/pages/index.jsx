26c05407371ddaced2bdd66e39ef47e736d96352
Norld <grymore@gmail.com>
2023-03-02 08:33:29 -0800

    no index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -25,7 +25,7 @@ export default function Home() {
         />
         <meta name="author" content="Made Supardi" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="robots" content="noindex" />
+        {/* <meta name="robots" content="noindex" /> */}
 
         <meta property="og:title" content="Digitera - Digital Agency" />
         <meta property="og:type" content="service.website" />
